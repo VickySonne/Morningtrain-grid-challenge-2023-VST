e3f7e60cbf99fed50ac0a10b6d75fbcde40ac6c5
VickySonne <vst1996@live.dk>
2023-04-02 16:30:19 +0200

    Frontpage slideshow added

--- reactfolder/src/pages/FrontPage.js
+++ reactfolder/src/pages/FrontPage.js
@@ -1,10 +1,34 @@
+import Abe from "../images/animals/abe.jpg";
+import Egern from "../images/animals/egern.jpg";
+import Elefant from "../images/animals/elefant.jpg";
+
 const FrontPage = () => {
+
+    const slideshowArray = [Abe, Egern, Elefant];
+
+    let i = 0;
+
+    function slideShow() {
+        let image = document.querySelector("#image");
+
+        image.src = slideshowArray[i];
+        
+        if(i >= slideshowArray.length - 1){
+            i=0;
+        } else {
+            i++;
+        }
+    };
+
+    setInterval(slideShow, 5000);
+
     return (
         <main className="frontpage-main-styling">
+            <img id="image" src={Elefant}/>
             <div className="container">
-                This is the front page with a slide show (like flicker) <br />
-                Should this be there on mobile version too? Yes, Centrer billede <br />
-                3 buttons at the bottom with shortcuts to the catagories
+                <button>Animals</button>
+                <button>Flowers</button>
+                <button>Interior Design</button>
             </div>
         </main>
     );
