94ce0142cda8a08b5d08dc6f0138066c466dab6d
VickySonne <vst1996@live.dk>
2023-04-03 11:41:00 +0200

    Sass folders and color variables created

--- reactfolder/src/index.js
+++ reactfolder/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './reset.css';
-import './index.scss';
+import './styles/reset.css';
+import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
