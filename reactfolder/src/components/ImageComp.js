217b8aebf3da65422286a0faee8d0298718e2ed4
VickySonne <vst1996@live.dk>
2023-04-03 22:48:09 +0200

    classNames added to images in list using variable

--- reactfolder/src/components/ImageComp.js
+++ reactfolder/src/components/ImageComp.js
@@ -6,7 +6,7 @@ const ImageComp = (props) => {
         <ul className="image-list">
             {array.map((array) => (
               <li key={array.id}>
-                <img lazy="true" src={array.src} alt={array.alt}/>
+                <img className={array.masonsize} lazy="true" src={array.src} alt={array.alt}/>
                 <div className="image-text">
                   <h4 className="image-title">{array.title}</h4>
                   <p className="image-owner">Fotograf: {array.owner}</p>
