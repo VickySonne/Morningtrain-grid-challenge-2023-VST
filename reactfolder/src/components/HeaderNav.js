f5c5961bd420989440dc0a760dd59183d778cdc7
VickySonne <vst1996@live.dk>
2023-04-04 23:59:35 +0200

    Small esthetic fixes

--- reactfolder/src/components/HeaderNav.js
+++ reactfolder/src/components/HeaderNav.js
@@ -7,7 +7,7 @@ const HeaderNav = () => {
         <header>
             <div className="header-content container">
                 <Link to="/"><p className="frontpage-button">Forside</p></Link>
-                <h2 className="header-name" >Vicky Sonne Thomsen</h2>
+                <h2 className="header-name" ><a href="https://sonnedesignsolutions.com/" target="_blank" rel="noreferrer">Vicky Sonne Thomsen</a></h2>
                 <CatagoryMenu />
             </div>
         </header>
